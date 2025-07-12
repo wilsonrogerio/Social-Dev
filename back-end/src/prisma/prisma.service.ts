30cf276c5937b7760859e9fd36287316010b9005
wilson <rajwii44@gmail.com>
2025-07-11 22:48:18 -0300

    adicionado onmoduledestroy/prisma service

--- back-end/src/prisma/prisma.service.ts
+++ back-end/src/prisma/prisma.service.ts
@@ -1,9 +1,14 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
-  }
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    //Inicializa o PrismaClient
+    async onModuleInit() {
+        await this.$connect();
+    }
+    //Desconecta o PrismaClient quando o módulo é destruído
+    async onModuleDestroy() {
+        await this.$disconnect();
+    }
 }
