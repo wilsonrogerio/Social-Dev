421ea06274f898777215509d98c41fd6163484c0
wilson <rajwii44@gmail.com>
2025-07-15 19:29:28 -0300

    criado LoginUserDto - instalado class-validator

--- back-end/src/auth/auth/auth.service.ts
+++ back-end/src/auth/auth/auth.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor( private prismaService: PrismaService) {}
+
+    Login( email: string, password: string ) {
+        try {
+            const user = this.prismaService.user.findUnique({
+                where: { email },
+            });
+
+            return user;
+        } catch (error) {
+            throw new HttpException('Login failed: ', HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
