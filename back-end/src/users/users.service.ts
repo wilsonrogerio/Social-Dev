8d3e0a5a7e02e45c5a30ef63be8696b642b0a5eb
wilson <rajwii44@gmail.com>
2025-07-16 08:42:40 -0300

    criado servico de hash de senha usando bycript

--- back-end/src/users/users.service.ts
+++ back-end/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -7,6 +7,16 @@ export class UsersService {
     constructor(private prismaService : PrismaService) {}
 
     async createUser(createUserDto: CreateUserDto) {
-        
+        try {
+            const emailExists = await this.prismaService.user.findUnique({
+                where: { email: createUserDto.email },})
+            if (emailExists) {
+                throw new HttpException('Email já cadastrado', HttpStatus.BAD_REQUEST);
+            }
+            
+           
+        } catch (error) {
+            throw new HttpException('Algo deu errado ao criar o usuário', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }   
 }
---
+++ back-end/src/utils/bycript/bycript/hash-bycript.ts
@@ -0,0 +1,4 @@
+export abstract class HashBycriptProtocol {
+    abstract hashPassword(password: string): Promise<string>;
+    abstract comparePassword(password: string, hash: string): Promise<boolean>;
+}
