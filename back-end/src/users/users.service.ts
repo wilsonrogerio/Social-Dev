4ffcc3fbec98f34079b3e4a8075cf5869ab14767
wilson <rajwii44@gmail.com>
2025-07-16 08:56:45 -0300

    criado servico de cirar usuario

--- back-end/src/users/users.service.ts
+++ back-end/src/users/users.service.ts
@@ -1,22 +1,39 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { BycriptService } from 'src/utils/bycript/bycript/bycript.service';
 
 @Injectable()
 export class UsersService {
-    constructor(private prismaService : PrismaService) {}
+    constructor(private prismaService: PrismaService, private hashService: BycriptService) { }
 
+    // Cria um novo usuário
     async createUser(createUserDto: CreateUserDto) {
         try {
+            // Verifica se o email já está cadastrado
             const emailExists = await this.prismaService.user.findUnique({
-                where: { email: createUserDto.email },})
+                where: { email: createUserDto.email },
+            })
             if (emailExists) {
+                // Se o email já existe, lança uma exceção
                 throw new HttpException('Email já cadastrado', HttpStatus.BAD_REQUEST);
             }
-            
-           
+            // Cria o hash da senha usando o serviço de hash
+            const hashedPassword = await this.hashService.hashPassword(createUserDto.password);
+            // Cria o usuário no banco de dados
+            const user = await this.prismaService.user.create({
+                data: {
+                    ...createUserDto,
+                    password: hashedPassword, // Usa a senha hasheada
+                },
+                select: {
+                    id: true, email: true, name: true, createdAt: true
+                }
+            });
+            return user; // Retorna o usuário criado
+
         } catch (error) {
             throw new HttpException('Algo deu errado ao criar o usuário', HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }   
+    }
 }
