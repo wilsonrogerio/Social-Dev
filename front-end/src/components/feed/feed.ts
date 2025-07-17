d00d5ff1b70518e05ce0d4f0ea9b9fc5c1ec17ac
Wilson Rogerio <rajwii44@gmail.com>
2025-07-17 19:18:25 -0300

    criado esquma basicos de pastas

---
+++ front-end/src/components/feed/feed.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feed',
+  imports: [],
+  templateUrl: './feed.html',
+  styleUrl: './feed.scss'
+})
+export class Feed {
+
+}
