b512dec270995e7bed90b86697251a39b56dd30d
Wilson Rogerio <rajwii44@gmail.com>
2025-07-17 20:48:18 -0300

    criadas rotas de login e registro

--- front-end/src/auth/auth-routing-module.ts
+++ front-end/src/auth/auth-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
