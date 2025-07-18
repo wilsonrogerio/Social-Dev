05fbd224ddfbaf797aba93ef02718fb206df96d1
Wilson Rogerio <rajwii44@gmail.com>
2025-07-18 11:33:13 -0300

    criada pagina de login/servico de login

--- front-end/src/utils/services/auth/auth.service.ts
+++ front-end/src/utils/services/auth/auth.service.ts
@@ -1,2 +1,15 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { IAuthLogin } from "../../interfaces/auth-interface/auth-login";
+
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthService {
+  private httpClientService = inject(HttpClient);
+
+  login(userLoginData: IAuthLogin) {
+    return this.httpClientService.post("", userLoginData);
+
+  }
 }
